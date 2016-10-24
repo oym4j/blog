4b6665801825d0f49089e3684749af38e10d04b1
qiangxie(谢强) <qiangxie@KUGOU.GZ>
2016-10-24 11:20:40 +0800

    init

---
+++ src/main/java/org/bumishi/techblog/api/Application.java
@@ -0,0 +1,16 @@
+package org.bumishi.techblog.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author qiang.xie
+ * @date 2016/10/24
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] arg){
+        SpringApplication.run(Application.class);
+    }
+}
