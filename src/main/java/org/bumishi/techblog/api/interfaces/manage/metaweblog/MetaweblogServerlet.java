6758e3d0bdafcc793ce41c9ee591f740db71d1ed
bumishi <1060420897@qq.com>
2017-04-10 20:57:34 +0800

    remove code

--- src/main/java/org/bumishi/techblog/api/interfaces/manage/metaweblog/MetaweblogServerlet.java
+++ src/main/java/org/bumishi/techblog/api/interfaces/manage/metaweblog/MetaweblogServerlet.java
@@ -1,8 +1,6 @@
 package org.bumishi.techblog.api.interfaces.manage.metaweblog;
 
 import org.apache.xmlrpc.XmlRpcException;
-import org.apache.xmlrpc.common.XmlRpcHttpRequestConfig;
-import org.apache.xmlrpc.server.AbstractReflectiveHandlerMapping;
 import org.apache.xmlrpc.server.PropertyHandlerMapping;
 import org.apache.xmlrpc.server.XmlRpcHandlerMapping;
 import org.apache.xmlrpc.webserver.XmlRpcServlet;
@@ -19,20 +17,10 @@ public class MetaweblogServerlet extends XmlRpcServlet{
     @Autowired
     private SpringRequestProcessorFactoryFactory springRequestProcessorFactoryFactory;
 
-    private boolean isAuthenticated(String pUserName, String pPassword) {
-        return "test".equals(pUserName) && "test".equals(pPassword);
-    }
+
     protected XmlRpcHandlerMapping newXmlRpcHandlerMapping() throws XmlRpcException {
         PropertyHandlerMapping mapping
                 = new PropertyHandlerMapping();
-        AbstractReflectiveHandlerMapping.AuthenticationHandler handler =
-                pRequest -> {
-                    XmlRpcHttpRequestConfig config1 =
-                            (XmlRpcHttpRequestConfig) pRequest.getConfig();
-                    return isAuthenticated(config1.getBasicUserName(),
-                            config1.getBasicPassword());
-                };
-        //mapping.setAuthenticationHandler(handler);
         mapping.setRequestProcessorFactoryFactory(springRequestProcessorFactoryFactory);
         mapping.addHandler("metaWeblog", MetaweblogApi.class);
         mapping.addHandler("blogger",MetaweblogApi.class);
